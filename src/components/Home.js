60d93453ebb5f5b206ed2b6e3fc8267ae0dc2c4a
Alice <nedumayu@gmail.com>
2021-10-04 17:16:42 +0300

    add files

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2 style={{display: 'flex', justifyContent: 'center'}}>
+                    Добро пожаловать в LETI pay!
+                </h2>
+            </div>
+        );
+    }
+}
+
+export default Home;
