a3766106d957102ed54c69e3a53532519c73200f
nastyyyya <0868662@mail.ru>
2021-10-21 21:37:32 +0300

    add RoleRepo

---
+++ src/main/java/org/example/letipay/repos/RoleRepo.java
@@ -0,0 +1,16 @@
+package org.example.letipay.repos;
+
+import org.example.letipay.models.Role;
+import org.example.letipay.models.RoleEnum;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+    Optional<Role> findByName(RoleEnum name);
+
+}
+
