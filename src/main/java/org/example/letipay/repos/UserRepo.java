8061ca12dac0897e37f323a478229a9b273591f3
nastyyyya <0868662@mail.ru>
2021-10-21 21:34:41 +0300

    modify UserRepo

--- src/main/java/org/example/letipay/repos/UserRepo.java
+++ src/main/java/org/example/letipay/repos/UserRepo.java
@@ -3,8 +3,13 @@ package org.example.letipay.repos;
 import org.example.letipay.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+
 }
