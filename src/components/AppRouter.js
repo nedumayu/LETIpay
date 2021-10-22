1c209c61a7cc8c7a5ed74f329c8ad93e3395e020
Alice <nedumayu@gmail.com>
2021-10-22 15:01:03 +0300

    add register & profile routes

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,22 +1,17 @@
 import React from 'react';
-import {Redirect, Route, Switch} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 import Home from "./Home";
 import Login from "./Login";
-import Main from "./Main";
+import Register from "./Register"
+import Profile from "./profile";
 
 const AppRouter = () => {
     return (
         <Switch>
-            <Route path="/home">
-                <Home/>
-            </Route>
-            <Route path="/login">
-                <Login/>
-            </Route>
-            <Route path="/main">
-                <Main/>
-            </Route>
-            <Redirect to="/home"/>
+            <Route exact path={["/", "/home"]} component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/profile" component={Profile} />
         </Switch>
     );
 };
