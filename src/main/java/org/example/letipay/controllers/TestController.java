c09e7f488f59f59af5ba6dd09ea40549374e6986
Alice <nedumayu@gmail.com>
2021-10-22 14:52:37 +0300

    update accesses

--- src/main/java/org/example/letipay/controllers/TestController.java
+++ src/main/java/org/example/letipay/controllers/TestController.java
@@ -3,6 +3,7 @@ package org.example.letipay.controllers;
 import org.example.letipay.models.User;
 import org.example.letipay.repos.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,14 +19,10 @@ public class TestController {
     @Autowired
     private UserRepository userRepository;
 
-   @GetMapping("/main")
-    public List<User> getUsers () {
+    @GetMapping("/profile")
+    @PreAuthorize("hasRole('USER') or hasRole('LETI') or hasRole('ADMIN')")
+    public List<User> userAccess() {
         return this.userRepository.findAll();
-    }
-
-    @GetMapping("/all")
-    public String allAccess() {
-        return "Публичная страница";
     }
 
 }
