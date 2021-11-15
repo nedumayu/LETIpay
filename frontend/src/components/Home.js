9b49109c2f485fa3d7180212af16ccac95d1f544
Alice <nedumayu@gmail.com>
2021-11-16 01:48:09 +0300

    remove registr and aut links

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 
 class Home extends React.Component {
     render() {
         return (
-            <div className="block">
-                <h2 style={{display: 'flex', justifyContent: 'center'}}>
+            <div>
+                <h2 style={{color: "#385b9a",display: 'flex', justifyContent: 'center', alignItems: 'center', height: '25vh'}}>
                     Добро пожаловать в LETI pay!
                 </h2>
-                <Link to="/login">Авторизоваться</Link>
-                <div></div>
-                <Link to="/register">Зарегистрироваться</Link>
             </div>
         );
     }
