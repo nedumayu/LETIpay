c558c5ed4e28dee412889b691a3750ee35c81e12
Alice <nedumayu@gmail.com>
2021-11-15 19:30:23 +0300

    add new profile conroller

---
+++ backend/src/main/java/org/example/letipay/controllers/ProfileController.java
@@ -0,0 +1,40 @@
+package org.example.letipay.controllers;
+
+import org.example.letipay.models.User;
+import org.example.letipay.repos.UserRepository;
+import org.example.letipay.securingweb.jwt.response.MessageResponse;
+import org.example.letipay.securingweb.jwt.response.ProfileResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/profile")
+public class ProfileController {
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping()
+    public ResponseEntity<?> getUserProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        /*User user = userRepository
+                .findByEmail(authentication.getName())
+                .orElse(null);
+        if(user != null) {
+            return ResponseEntity.ok(
+                    new ProfileResponse(user));
+        }
+        else {*/
+            return ResponseEntity.badRequest()
+                    .body(
+                            new MessageResponse("User not found!")
+                    );
+        }
+    /*}*/
+}
