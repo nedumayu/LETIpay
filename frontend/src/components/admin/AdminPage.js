fabf1fa00c510776092a4e2a987fd82df4b5de9e
Alice <nedumayu@gmail.com>
2021-11-21 23:14:36 +0300

    show users table with delete buttons

---
+++ frontend/src/components/admin/AdminPage.js
@@ -0,0 +1,23 @@
+import React, {useEffect, useState} from "react";
+import Modal from "../modal/Modal";
+import AdminTable from "./AdminTable";
+import "./table.css"
+import AddNewUser from "./AddNewUser";
+
+const AdminPage = () => {
+    const [modalActive, setModalActive] = useState(false);
+
+    return(
+        <div className="table-container">
+            <button className="my-button" onClick={() => setModalActive(true)}>
+                Добавить пользователя
+            </button>
+            <Modal active={modalActive} setActive={setModalActive}>
+                <AddNewUser setActive={setModalActive}/>
+            </Modal>
+            <AdminTable />
+        </div>
+    )
+}
+
+export default AdminPage;
