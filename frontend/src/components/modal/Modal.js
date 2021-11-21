ba1c2521bf4b0ae1111ca7902e16748c0201a7ef
Alice <nedumayu@gmail.com>
2021-11-21 23:12:51 +0300

    add modal window component

---
+++ frontend/src/components/modal/Modal.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./Modal.css"
+
+const Modal = ({active, setActive, children}) => {
+    return (
+        <div className={active ? "modal_ active" : "modal_"}>
+            <div className={active ? "modal-content_ active" : "modal-content_"} onClick={event => event.stopPropagation()}>
+                <button
+                    type="button"
+                    className="btn-close-modal"
+                    data-dismiss="modal"
+                    aria-label="Close"
+                    onClick={() => setActive(false)}>
+                    <span aria-hidden="true">&times;</span>
+                </button>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
