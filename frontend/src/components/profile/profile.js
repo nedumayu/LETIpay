95278d956b6d532b860455520ebef5e240ae3fbf
Alice <nedumayu@gmail.com>
2021-11-22 22:46:40 +0300

    remove static card example from profile

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -3,7 +3,7 @@ import UserService from "../../services/UserService";
 import StorageService from "../../services/StorageService";
 import "./profile.css"
 import profileIcon from "../../assets/profile-icon-1.png"
-import plasticCard from "../../assets/plastic-card.png"
+import CardsList from "./CardsList";
 
 
 export default class Profile extends Component {
@@ -16,7 +16,8 @@ export default class Profile extends Component {
     componentDidMount() {
         const currentUser = StorageService.getUser();
         UserService.getUserProfile(currentUser.id).then(
-            response => { const userData = response.data;
+            response => {
+                const userData = response.data;
                 this.setState({
                     userData: userData,
                     currentUser: currentUser,
@@ -35,22 +36,12 @@ export default class Profile extends Component {
                             <img className="avatar" src={profileIcon} alt="Profile icon"/>
                             <div className="info">
                                 <h4 className="name">{userData.username}</h4>
-                                <p><b>Группа</b> №{userData.groupNumber}</p>
+                                <p><b>Группа №</b>{userData.groupNumber}</p>
                                 <p><b>E-mail:</b> {userData.email}</p>
                                 <p><b>Телефон:</b> {userData.telephone}</p>
                             </div>
                         </div>
-                            <div className="cards">
-                                <button className="my-button">Добавить карту</button>
-                                <div className="my-card">
-                                    <div className="card-img"><img className="card-icon" src={plasticCard} alt="Card icon"/></div>
-                                    <div className="card-info">
-                                        <h6 className="card-name">MasterCard Mass</h6>
-                                        <p className="card-number">**** 9037</p>
-                                    </div>
-                                    <div><p className="card-cost">1000 Р</p></div>
-                                </div>
-                            </div>
+                        <CardsList/>
                     </div> : null}
             </div>
         );
