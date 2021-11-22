c918b2aa65a2a9bc7767949a8f9ace44744e369c
Alice <nedumayu@gmail.com>
2021-11-22 22:57:01 +0300

    upgrade user service with new methods for post and get card

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -12,6 +12,16 @@ class UserService {
     deleteUser(id) {
         return api.delete(`test/admin/${id}`, {data: {id: id}})
     }
+
+    addCard(cardData) {
+        return api.post('profile', cardData )
+    }
+
+    getCardInfo(id) {
+        return api.get('profile/card', { params: { id: id} });
+    }
+
+
 }
 
 export default new UserService()
