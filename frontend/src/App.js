15affc1db080c04fae5ee7330d1465f101f09f9e
Alice <nedumayu@gmail.com>
2021-11-23 02:34:44 +0300

    add bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import "./App.css";
 import Navbar from "./components/nav/navbar"
 
