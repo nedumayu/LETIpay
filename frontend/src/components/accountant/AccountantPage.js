e3a9f9b05a5816facbb2f6295810ab37f93633ba
Alice <nedumayu@gmail.com>
2021-12-04 02:03:29 +0300

    try payments to display

--- frontend/src/components/accountant/AccountantPage.js
+++ frontend/src/components/accountant/AccountantPage.js
@@ -1,43 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
 import UserService from "../../services/UserService";
 import "./accountantTable.css"
 
-export default class AccountantPage extends Component {
-    state = {
-        payments: []
+class AccountantPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            payments: []
+        }
     }
+
 
     componentDidMount() {
         UserService.getAccountantInfo()
             .then(res => {
-                const payments = res.data;
-                this.setState({payments});
+                this.setState({payments: res.data});
             })
     }
 
+
     render() {
-        console.log(this.state.payments);
         return (
             <div className="table-container">
                 <h3 style={{marginTop: "20px"}}>Список платежей</h3>
                 <table className="account-table">
                     <thead>
                     <tr>
-                        <td>Id платежа</td>
-                        <td>Название оплаты</td>
-                        <td>Сумма оплаты</td>
-                        <td>Дата оплаты</td>
-                        <td>id карты</td>
+                        <td>Id</td>
+                        <td>Название</td>
+                        <td>Сумма</td>
+                        <td>Дата</td>
                     </tr>
                     </thead>
                     <tbody>{
-                        this.state.payments.map(payment =>
+                        this.state.payments.map(
+                            payment =>
                             <tr key={payment.id}>
                                 <td>{payment.id}</td>
                                 <td>{payment.payName}</td>
                                 <td>{payment.paySum}</td>
                                 <td>{payment.payDate}</td>
-                                <td>{payment.card}</td>
                             </tr>
                         )}
                     </tbody>
@@ -45,4 +48,63 @@ export default class AccountantPage extends Component {
             </div>
         )
     }
+    /*state = {
+        cardData: []
+    }
+
+    componentDidMount() {
+        UserService.getAccountantInfo()
+            .then(res => {
+                const cardData = res.data;
+                this.setState({cardData});
+            })
+    }
+
+
+    delete = (id) => {
+        UserService.deleteUser(id).then(
+            response => {
+                window.location.reload()
+            },
+            error => {
+                console.log(error.response)
+            }
+        )
+    }
+
+    render() {
+        console.log(this.state.cardData);
+        return (
+            <div>
+                <h3 style={{marginTop: "20px"}}>Список пользователей</h3>
+                <table className="mtable">
+                    <thead>
+                    <tr>
+
+                        <td>card name</td>
+                        <td>owner</td>
+                        <td>Номер card</td>
+                        <td>end date</td>
+                        <td>check</td>
+                        <td>Action</td>
+                    </tr>
+                    </thead>
+                    <tbody>{
+                        this.state.cardData.map(cardData =>
+                            <tr key={cardData.cardName}>
+
+                                <td>{cardData.cardName}</td>
+                                <td>{cardData.cardOwner}</td>
+                                <td>{cardData.cardNumber}</td>
+                                <td>{cardData.endDate}</td>
+                                <td>{cardData.cardCheck}</td>
+                                <td><button className="delete-button" style={{margin: "5px"}} onClick={() => this.delete(cardData.id)}>Delete</button></td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }*/
 }
+export default AccountantPage;
