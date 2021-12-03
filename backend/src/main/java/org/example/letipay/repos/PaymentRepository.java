156bf2e1e9a06871dba2bb1919abb7b7a875eb18
Alice <nedumayu@gmail.com>
2021-12-04 01:57:36 +0300

    try to display payments right

--- backend/src/main/java/org/example/letipay/repos/PaymentRepository.java
+++ backend/src/main/java/org/example/letipay/repos/PaymentRepository.java
@@ -1,12 +1,21 @@
 package org.example.letipay.repos;
 
+import org.example.letipay.models.Card;
 import org.example.letipay.models.Payment;
+import org.example.letipay.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
+    @Query("select p from Payment p join p.card c where c.user = :user")
+    List<Payment> findPaymentByCard(User user);
+
+    @Query("select p, c from Payment p join p.card c")
+    List<Payment> findAllPayments();
 }
