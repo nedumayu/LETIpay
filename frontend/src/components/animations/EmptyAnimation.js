143f7b1417026402caf77296ef0cc4e502eb9551
Alice <nedumayu@gmail.com>
2021-12-13 19:09:42 +0300

    add new animations from lottie

---
+++ frontend/src/components/animations/EmptyAnimation.js
@@ -0,0 +1,8 @@
+import Lottie from "lottie-react";
+import Banking from "../../assets/empty-file.json";
+
+const EmptyAnimation = () => {
+    return <Lottie animationData={Banking} />;
+};
+
+export default EmptyAnimation;
--- frontend/src/components/animations/ErrorAnimation.js
+++ frontend/src/components/animations/ErrorAnimation.js
@@ -1,8 +1,8 @@
 import Lottie from "lottie-react";
-import Banking from "../../assets/banking.json"
+import Meeting from "../../assets/error-404-page.json"
 
 const BankingAnimation = () => {
-    return <Lottie animationData={Banking} />;
+    return <Lottie animationData={Meeting} />;
 };
 
 export default BankingAnimation;
