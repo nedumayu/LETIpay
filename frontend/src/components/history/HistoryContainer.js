1cdc4c02ed16403d0abd61160ec9b0d90053fb3d
Alice <nedumayu@gmail.com>
2021-12-13 19:15:22 +0300

    add no operations message for history page

--- frontend/src/components/history/HistoryContainer.js
+++ frontend/src/components/history/HistoryContainer.js
@@ -1,8 +1,47 @@
 import PayHistory from "./PayHistory";
 import TransHistory from "./TransHistory";
+import React, {useEffect, useState} from "react";
+import UserService from "../../services/UserService";
+import NoResultAnimation from "../animations/NoResultAnimation";
 
 const HistoryContainer = () => {
+
+    const [PaymentsIsNull, setPaymentsIsNull] = useState(true);
+    const [TransfersIsNull, setTransfersIsNull] = useState(true);
+
+    useEffect(() => {
+        fetchingPayments();
+        fetchTransfers();
+    }, [])
+
+    const fetchingPayments = () => {
+        UserService.getUserPay().then(
+            response => {
+                const pays = response.data;
+                if (pays.length !== 0) {
+                    setPaymentsIsNull(false);
+                }
+            })
+    }
+
+    const fetchTransfers = () => {
+        UserService.getUserTrans().then(
+            response => {
+                const trans = response.data;
+                if (trans.length !== 0) {
+                    setTransfersIsNull(false);
+                }
+        })
+    }
+
+
     return(
+        (PaymentsIsNull && TransfersIsNull)===true ?
+            <div style={{width: "600px", margin: "20px auto"}}>
+                <h3 style={{textAlign: "center"}}>Еще не совершено ни одной операции</h3>
+                <div style={{width: "400px", margin: "0 auto"}}><NoResultAnimation/></div>
+            </div> :
+
         <div style={{width: "1200px", display: "flex", justifyContent: "space-between", margin: "20px auto"}}>
             <PayHistory/>
             <TransHistory/>
