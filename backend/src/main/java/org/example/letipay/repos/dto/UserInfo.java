2eac36cedd55408568f0e9fb522aa0cdbb146bc2
Alice <nedumayu@gmail.com>
2021-12-13 18:39:21 +0300

    add payment and user info

---
+++ backend/src/main/java/org/example/letipay/repos/dto/UserInfo.java
@@ -0,0 +1,13 @@
+package org.example.letipay.repos.dto;
+
+public class UserInfo {
+    private final String username;
+
+    public UserInfo(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
