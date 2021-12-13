4511c3f4447ea6a879cc4019b16c3ca01301c1ef
Alice <nedumayu@gmail.com>
2021-12-13 18:40:42 +0300

    add working query for payments

--- backend/src/main/java/org/example/letipay/repos/PaymentRepository.java
+++ backend/src/main/java/org/example/letipay/repos/PaymentRepository.java
@@ -3,6 +3,7 @@ package org.example.letipay.repos;
 import org.example.letipay.models.Card;
 import org.example.letipay.models.Payment;
 import org.example.letipay.models.User;
+import org.example.letipay.repos.dto.PaymentInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -18,4 +19,9 @@ public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
     @Query("select p, c from Payment p join p.card c")
     List<Payment> findAllPayments();
+
+    @Query("select new org.example.letipay.repos.dto.PaymentInfo(p.id, p.payName, p.paySum, p.payDate, u.username) " +
+            "from Payment p inner join User u on (p.card=u.id)")
+    List<PaymentInfo> findPaymentsAndUsers();
+
 }
