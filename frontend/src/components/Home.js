de2269478b5224d74662aa1bf33d95fed9ba7d6d
Alice <nedumayu@gmail.com>
2021-12-13 18:56:51 +0300

    fix animation width

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,13 +5,13 @@ class Home extends React.Component {
     render() {
         return (
             <div className="home-page">
-                <div>
+                <div style={{marginRight: "100px"}}>
                     <h1 style={{fontWeight: "800", fontSize: "50px", marginBottom: "50px"}} >Добро пожаловать в <span style={{color: "#347CE1"}}>LETIpay</span>!</h1>
                     <p>Сервис, разработанный специально для студентов ЛЭТИ</p>
-                    <p>Приложение позволит легко и быстро совершать переводы<br/>
+                    <p>Приложение позволит легко и быстро совершать переводы
                         и производить своевременную оплату обучения</p>
                 </div>
-                <div style={{width: "600px"}}>
+                <div style={{width: "800px"}}>
                     <BankingAnimation/>
                 </div>
             </div>
